70ebf482e13162299f6e2c757a43f7029dab91f2
Michael Skogberg <malliina123@gmail.com>
2020-03-26 00:19:05 +0200

    Add project files.

---
+++ AppWPF/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        State state = new State { LabelText = "Press the button!" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = state;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            state.LabelText = "You pressed it!";
+        }
+    }
+
+    public class State : INotifyPropertyChanged
+    {
+        private string labelText;
+        public string LabelText
+        {
+            get { return labelText; }
+            set
+            {
+                labelText = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
